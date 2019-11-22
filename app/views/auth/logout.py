9fd931439f4719a92e98998f25c72ea51905b446
YiShan8787 <59713684gx@gmail.com>
2019-11-22 11:48:59 +0800

    update login form

--- app/views/auth/logout.py
+++
@@ -1,8 +0,0 @@
-from flask import request, redirect
-from flask.views import MethodView
-from flask_login import login_required, current_user, logout_user
-
-class LogoutView(MethodView):
-    def get(self):
-        logout_user()
-        return redirect('/login')
