9a80a559bb4e1539dac1b2ebd1e3c4f5e4a087fa
Jimmy1244 <a0976625415@gmail.com>
2019-11-26 19:48:12 +0800

    reverse proxy test

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,8 @@ from flask_bootstrap import Bootstrap
 from flask_bcrypt import Bcrypt
 from flask_ckeditor import CKEditor
 from flask_mail import Mail
+#from flask_reverse_proxy_fix.middleware import ReverseProxyPrefixFix
+from flask_behind_proxy import FlaskBehindProxy
 
 app = Flask(__name__)
 app.config.from_pyfile('../config.cfg')
@@ -16,3 +18,7 @@ bootstrap = Bootstrap(app)
 bcrypt = Bcrypt(app)
 ckeditor = CKEditor(app)
 mail = Mail(app)
+
+app.config['REVERSE_PROXY_PATH'] = '/hishop'
+#ReverseProxyPrefixFix(app)
+proxied = FlaskBehindProxy(app)
