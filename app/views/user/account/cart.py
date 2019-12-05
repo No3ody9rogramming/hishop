0bbf2bfcac5d7cd79ec527d9d19f193694ff5c0c
Jimmy1244 <a0976625415@gmail.com>
2019-12-05 19:19:46 +0800

    fetch checked product_id in cart

--- app/views/user/account/cart.py
+++ app/views/user/account/cart.py
@@ -19,31 +19,32 @@ class CartView(MethodView):
         return render_template('user/account/cart.html', form=form, hicoin=current_user.hicoin, products = products)
     
     def post(self):
-    	pass
-    #     form = CartForm()
+        form = CartForm()
 
-    #     if form.validate_on_submit():
-    #         print(url_for('static', filename='hicoin.png', _external=True))
-    #         headers = {"X-LINE-ChannelId": app.config['CHANNEL_ID'],
-    #                    "X-LINE-ChannelSecret": app.config['CHANNEL_SECRET'],
-    #                    "Content-Type": "application/json; charset=UTF-8"}
-    #         data = {"amount": form.payment.data,
-    #                 "productName": "Hi幣",
-    #                 "productImageUrl": url_for('static', filename='hicoin.png', _external=True),
-    #                 "confirmUrl": url_for('payment_confirm', _external=True),
-    #                 "orderId": uuid.uuid4().hex,
-    #                 "currency": "TWD"}
-    #         response = requests.post(app.config['CHANNEL_URL'], headers=headers, data=json.dumps(data).encode('utf-8'))
+        checkedProducts = request.form.getlist("productCheck")
+        return str(checkedProducts);
+        # if form.validate_on_submit():
+        #     print(url_for('static', filename='hicoin.png', _external=True))
+        #     headers = {"X-LINE-ChannelId": app.config['CHANNEL_ID'],
+        #                "X-LINE-ChannelSecret": app.config['CHANNEL_SECRET'],
+        #                "Content-Type": "application/json; charset=UTF-8"}
+        #     data = {"amount": form.payment.data,
+        #             "productName": "Hi幣",
+        #             "productImageUrl": url_for('static', filename='hicoin.png', _external=True),
+        #             "confirmUrl": url_for('payment_confirm', _external=True),
+        #             "orderId": uuid.uuid4().hex,
+        #             "currency": "TWD"}
+        #     response = requests.post(app.config['CHANNEL_URL'], headers=headers, data=json.dumps(data).encode('utf-8'))
             
-    #         if int(json.loads(response.text)['returnCode']) == 0:
-    #             payment = Payment(payer_id=current_user.id,
-    #                               transaction_id = json.loads(response.text)['info']['transactionId'],
-    #                               amount=form.payment.data,
-    #                               currency="TWD",
-    #                               confirm=False).save()
-    #             return redirect(json.loads(response.text)['info']['paymentUrl']['web'])
+        #     if int(json.loads(response.text)['returnCode']) == 0:
+        #         payment = Payment(payer_id=current_user.id,
+        #                           transaction_id = json.loads(response.text)['info']['transactionId'],
+        #                           amount=form.payment.data,
+        #                           currency="TWD",
+        #                           confirm=False).save()
+        #         return redirect(json.loads(response.text)['info']['paymentUrl']['web'])
 
-    #     return redirect(url_for('payment'))
+        # return redirect(url_for('payment'))
 
 
 class CartForm(FlaskForm):
