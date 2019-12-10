959b76faaa56dfa777e9dd014b0fe87a113b850f
Jimmy1244 <a0976625415@gmail.com>
2019-12-10 23:04:32 +0800

    hichat work with db

--- app/views/user/hiChat.py
+++ app/views/user/hiChat.py
@@ -5,24 +5,31 @@ from flask_login import current_user
 from flask_socketio import emit ##for test socketio
 
 from app.models.user import User
+from app.models.message import Message
 from app import app, socketio
+
+import json
 
 class HiChatView(MethodView):
     def get(self):
-        # if request.args.get('way') == "bidding":
-        #     products = Product.objects(name__icontains=request.args.get('keyword'), status=0, bidding=True)
-            
-        #     way = "bidding"
-        # elif request.args.get('way') == "normal":
-        #     products = Product.objects(name__icontains=request.args.get('keyword'), status=0, bidding=False)
-        #     way = "normal"
-        # else:
-        #     abort(404)
-
-        return render_template('user/hichatT.html', app=app, users=User.objects, currentUserID=current_user.id)
+        return render_template('user/hichatT.html', app=app, users=User.objects, currentUser=current_user)
 
     def post(self):
-    	pass
+        senderID = request.values["senderID"]
+        receiverID = request.values["receiverID"]        
+        if senderID != str(current_user.id):
+            print(senderID)
+            print(current_user.id)
+            abort(403)        
+        messages = Message.objects(sender_id=senderID, receiver_id=receiverID).to_json()
+        if receiverID != senderID:
+            messagesRtoS = Message.objects(sender_id=receiverID, receiver_id=senderID)
+            if(messagesRtoS.count() > 0 ):
+                messagesList = json.loads(messages)
+                messagesRtoSList = json.loads(messagesRtoS.to_json())
+                messagesList.append(messagesRtoSList);
+                messages = json.dumps(messagesList)
+        return messages;
 
 @socketio.on('chat message')
 def handle_message(senderID, receiverID, message):
@@ -33,4 +40,7 @@ def handle_message(senderID, receiverID, message):
         "message" : message
     }
     emit(receiverID, data, broadcast=True)
-    emit(senderID, data, broadcast=True)
+    if receiverID != senderID:
+        emit(senderID, data, broadcast=True)
+    message = Message(sender_id=senderID, receiver_id=receiverID, message=message)
+    message.save()
