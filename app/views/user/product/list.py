3c3ae52dc73fdb6ab6f8ec2aaf74794e159dc663
YiShan8787 <59713684gx@gmail.com>
2019-12-11 19:25:52 +0800

    list

--- app/views/user/product/list.py
+++ app/views/user/product/list.py
@@ -3,12 +3,26 @@ from flask.views import MethodView
 from flask_login import current_user, login_required
 
 from app.models.order import Order
+from app.models.product import Product
 
 #移交中 領收中 已完成 已取消 全部
 ORDER_STATUS = {"TRANSFERING" : "0", "RECEIPTING" : "1", "COMPLETE" : "2", "CANCEL" : "3", "ALL" : "4"}
 class PurchaseListView(MethodView):
     def get(self):
         status = request.args.get('status')
+
+        test = Order.objects.aggregate(*[
+            {
+                '$lookup':
+                {
+                    'from': 'Product',
+                    'localField': 'product_id',
+                    'foreignField': 'id',
+                    'as':'order2product',
+                }
+            },
+        ])
+
         if status == ORDER_STATUS['TRANSFERING']:
             orders = Order.objects(buyer_id=current_user.id, status=ORDER_STATUS["TRANSFERING"])
         elif status == ORDER_STATUS['RECEIPTING']:
@@ -22,6 +36,9 @@ class PurchaseListView(MethodView):
             orders = Order.objects(buyer_id=current_user.id)
 
         orders = sorted(orders, key=lambda k: k.create_time, reverse=False)
+        
+
+        print(orders)
 
         return render_template('user/product/list.html', orders=orders, ORDER_STATUS=ORDER_STATUS, status=status)
     def post(self):
