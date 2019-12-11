1ca50582a107f448ba463a8c8b88ae7368692bf9
HoKuanYu <40305418+HoKuanYu@users.noreply.github.com>
2019-12-11 22:30:09 +0800

    Update all_question to questionList

--- app/views/user/question/list.py
+++ app/views/user/question/list.py
@@ -5,10 +5,10 @@ from flask_login import current_user, login_required
 from app.models.question import Question
 
 
-class All_questionView(MethodView):
+class QuestionListView(MethodView):
     def get(self):
-        questions = Question.objects()
+        questions = Question.objects(user_id=current_user.id)
 
-        return render_template('user/question/all_question.html', questions=questions)
+        return render_template('user/question/list.html', questions=questions)
     def post(self):
     	pass
