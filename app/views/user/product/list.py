cffb839dde8587368b6e61450bfa5929c8ef62ee
YiShan8787 <59713684gx@gmail.com>
2019-12-14 22:42:19 +0800

    list

--- app/views/user/product/list.py
+++ app/views/user/product/list.py
@@ -1,6 +1,9 @@
 from flask import redirect, render_template, url_for, request
 from flask.views import MethodView
 from flask_login import current_user, login_required
+from wtforms import SubmitField, TextAreaField
+from flask_wtf import FlaskForm
+from wtforms.validators import DataRequired, InputRequired, Length, EqualTo, ValidationError
 
 from app.models.order import Order
 from app.models.product import Product
@@ -10,6 +13,7 @@ from app.models.product import Product
 ORDER_STATUS = {"TRANSFERING" : "0", "RECEIPTING" : "3", "COMPLETE" : "2", "CANCEL" : "1", "ALL" : "4"}
 class PurchaseListView(MethodView):
     def get(self):
+        form = PerchaseListForm()
         status = request.args.get('status')
 
         '''test = Order.objects.aggregate(*[
@@ -39,6 +43,15 @@ class PurchaseListView(MethodView):
         orders = sorted(orders, key=lambda k: k.create_time, reverse=False)
 
 
-        return render_template('user/product/list.html', orders=orders, ORDER_STATUS=ORDER_STATUS, status=status)
+        return render_template('user/product/list.html', orders=orders, ORDER_STATUS=ORDER_STATUS, status=status, form=form)
     def post(self):
-        pass
+        form = PerchaseListForm()
+        if form.validate_on_submit():
+            #need to be write something
+            return redirect(url_for('user.perchase_list'))
+        return render_template('user/product/list.html', form=form)
+
+# for comment
+class PerchaseListForm(FlaskForm):
+    detail = TextAreaField("評論", render_kw={'rows': 7}, validators=[InputRequired(), Length(max=4000)])
+    submit = SubmitField('提交')
