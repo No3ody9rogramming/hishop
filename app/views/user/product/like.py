268c62dd067f995f7a1bbe7b289e6edb08befb58
YiShan8787 <59713684gx@gmail.com>
2019-12-17 18:18:28 +0800

    like

--- app/views/user/product/like.py
+++ app/views/user/product/like.py
@@ -5,11 +5,11 @@ from flask_login import current_user, login_required
 from app.models.product import Product
 from app.models.information import Information
 
-
+PRODUCT_STATUS = {"SELLING" : "0", "SOLD" : "1", "FROZEN" : "2", "REMOVE" : "3", "ALL" : "4"}
 class LikeView(MethodView):
     def get(self):
         products = Information.objects(user_id=current_user.id).first().like
 
-        return render_template('user/product/like.html', products=products)
+        return render_template('user/product/like.html', products=products,PRODUCT_STATUS=PRODUCT_STATUS)
     def post(self):
     	pass
