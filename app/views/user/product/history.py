32aa8e48c489d99e340e7d520cafefa1cd98f3f3
YiShan8787 <59713684gx@gmail.com>
2019-12-17 18:36:40 +0800

    history

--- app/views/user/product/history.py
+++ app/views/user/product/history.py
@@ -5,13 +5,13 @@ from flask_login import current_user, login_required
 from app.models.product import Product
 from app.models.information import Information
 
-
+PRODUCT_STATUS = {"SELLING" : "0", "SOLD" : "1", "FROZEN" : "2", "REMOVE" : "3", "ALL" : "4"}
 class HistoryView(MethodView):
     def get(self):
         products = Information.objects(user_id=current_user.id).first().history
 
         products = sorted(products, key=lambda k: k.create_time, reverse=False)
 
-        return render_template('user/product/history.html', products=products)
+        return render_template('user/product/history.html', products=products,PRODUCT_STATUS=PRODUCT_STATUS)
     def post(self):
     	pass
