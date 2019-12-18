c3eaed5cc969a5084f1b073ef7cb9f8860af70c7
YiShan8787 <59713684gx@gmail.com>
2019-12-18 15:29:23 +0800

    like history like

--- app/views/user/product/like.py
+++ app/views/user/product/like.py
@@ -1,4 +1,4 @@
-from flask import redirect, render_template, url_for, abort
+from flask import redirect, render_template, url_for, abort, request
 from flask.views import MethodView
 from flask_login import current_user, login_required
 
@@ -6,10 +6,12 @@ from app.models.product import Product
 from app.models.information import Information
 
 PRODUCT_STATUS = {"SELLING" : "0", "SOLD" : "1", "FROZEN" : "2", "REMOVE" : "3", "ALL" : "4"}
+SORT_STATUS = {"PRICE":"0","HOT":"1","RATING":"2","CREATE_TIME":"3"}
 class LikeView(MethodView):
     def get(self):
-        products = Information.objects(user_id=current_user.id).first().like
+        status = request.args.get('status')
+        products = Information.objects(user_id=current_user.id).order_by(status).first().like
 
-        return render_template('user/product/like.html', products=products,PRODUCT_STATUS=PRODUCT_STATUS)
+        return render_template('user/product/like.html', products=products,PRODUCT_STATUS=PRODUCT_STATUS,SORT_STATUS=SORT_STATUS)
     def post(self):
     	pass
