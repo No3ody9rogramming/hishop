ad8a1704419b6f162d6d05fdd0a2cd3f2f0960f6
Jimmy1244 <a0976625415@gmail.com>
2019-12-28 05:59:24 +0000

    frontend of hichat

--- app/views/user/hiChat/view.py
+++ app/views/user/hiChat/view.py
@@ -1,6 +1,4 @@
-import datetime
-
-from flask import redirect, render_template, url_for, flash, request, abort
+from flask import render_template, request
 from flask.views import MethodView
 from flask_login import current_user
 
@@ -9,13 +7,17 @@ from app.models.message import Message
 
 from mongoengine.queryset.visitor import Q
 
+
 class HiChatView(MethodView):
     def get(self):
-        messagesOwners = Message.objects((Q(sender_id=current_user.id)) | (Q(receiver_id=current_user.id))).order_by("-create_time").aggregate(
+        messagesOwners = Message.objects(
+            (Q(sender_id=current_user.id)) |
+            (Q(receiver_id=current_user.id))
+            ).order_by("-create_time").aggregate(
             {
                 '$lookup': {
                     'from': 'user',
-                    'localField': 'sender_id' ,
+                    'localField': 'sender_id',
                     'foreignField': '_id',
                     'as': 'userS'
                 }
@@ -23,7 +25,7 @@ class HiChatView(MethodView):
             {
                 '$lookup': {
                     'from': 'user',
-                    'localField': 'receiver_id' ,
+                    'localField': 'receiver_id',
                     'foreignField': '_id',
                     'as': 'userR'
                 }
@@ -36,7 +38,7 @@ class HiChatView(MethodView):
             {
                 '$lookup': {
                     'from': 'user',
-                    'localField': 'current_user' ,
+                    'localField': 'current_user',
                     'foreignField': '_id',
                     'as': 'userC'
                 }
@@ -49,7 +51,7 @@ class HiChatView(MethodView):
                                 'userSS': '$userS',
                                 'userRR': '$userR'
                             },
-                            'in': { '$setUnion': [ '$$userSS', '$$userRR' ] }
+                            'in': {'$setUnion': ['$$userSS', '$$userRR']}
                         }
                     }
                 }
@@ -57,13 +59,17 @@ class HiChatView(MethodView):
             {
                 '$group': {
                     '_id': {
-                        '$setDifference': [ '$userSR', '$userC']
+                        '$setDifference': ['$userSR', '$userC']
                     },
                     'create_time': {
                         '$max': '$create_time'
                     },
-                    'messages': { 
-                        '$push':  { 'message': "$message", 'isRead': "$isRead" }
+                    'messages': {
+                        '$push':  {
+                            'message': "$message",
+                            'isRead': "$isRead",
+                            'sender': "$userS.name"
+                        }
                     }
                 }
             },
@@ -87,11 +93,14 @@ class HiChatView(MethodView):
                 }
                 u['_id'].append(dummyCurrentUserDict)
 
-        users = sorted(users, key = lambda i: i['create_time'], reverse=True)
+        users = sorted(users, key=lambda i: i['create_time'], reverse=True)
 
         return render_template('user/hichatT.html', Tusers=User.objects, users=users, currentUser=current_user)
 
     def post(self):
         receiverID = request.values["receiverID"]
-        messages = Message.objects((Q(sender_id=current_user) & Q(receiver_id=receiverID)) | (Q(sender_id=receiverID) & Q(receiver_id=current_user))).order_by("+create_time")
+        messages = Message.objects(
+            (Q(sender_id=current_user) & Q(receiver_id=receiverID)) |
+            (Q(sender_id=receiverID) & Q(receiver_id=current_user))
+            ).order_by("+create_time")
         return messages.to_json()
