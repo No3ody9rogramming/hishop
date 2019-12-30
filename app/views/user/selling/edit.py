f33366c54e635c2378bea875755e021e94c6f332
YiShan8787 <59713684gx@gmail.com>
2019-12-30 23:03:38 +0800

    image edit

--- app/views/user/selling/edit.py
+++ app/views/user/selling/edit.py
@@ -7,7 +7,7 @@ from flask_wtf import FlaskForm
 from flask_wtf.file import FileRequired, FileAllowed
 from wtforms import StringField, SubmitField, IntegerField, FileField, HiddenField
 from wtforms.fields.html5 import DateTimeLocalField
-from wtforms.validators import InputRequired, Length, EqualTo, ValidationError, NumberRange
+from wtforms.validators import InputRequired, Length, EqualTo, ValidationError, NumberRange, Optional
 
 from app import bcrypt
 from app.models.product import Product, Bid
@@ -23,6 +23,7 @@ class EditView(MethodView):
         form.product_id.data = request.values["product_id"]
         form.price.data = product.price
         form.name.data = product.name
+        
         form.detail.data=product.detail
         categories = Category.objects()
 
@@ -38,13 +39,19 @@ class EditView(MethodView):
             product.name=form.name.data
             product.price=form.price.data
             product.detail=form.detail.data
-            #product.image="product." + form.image.data.filename.split(".")[1].lower()
-            #product.categories=request.form.getlist("categories")
+            print(form.image.data.filename)
+            if(form.image.data.filename!=''):
+                image_path = os.path.join(os.getcwd(), 'app/static/image', str(product.id))
+                print(product.image)
+                try:
+                    os.remove(os.path.join(image_path, product.image))   #刪掉原本的檔案
+                    print("File is deleted successfully")
+                    
+                except:
+                    print(e)
+                product.image = "product." + form.image.data.filename.split('.')[-1].lower()  #上傳成功設定圖片名稱
+                form.image.data.save(os.path.join(image_path, product.image)) #儲存上傳的檔案
 
-            #image_path = os.path.join(os.getcwd(), 'app/static/image', str(product.id))
-            #os.makedirs(image_path)
-            #form.image.data.save(os.path.join(image_path, product.image))
-            
             product.save()
 
             return redirect(url_for('user.selling_list'))
@@ -53,7 +60,7 @@ class EditView(MethodView):
 
 class EditForm(FlaskForm):
     product_id = HiddenField("", validators=[InputRequired()])
-    #image = FileField("商品照片", validators=[FileRequired(), FileAllowed(['jpeg', 'jpg', 'png', 'gif'], '只能上傳圖片(.jpg, .jpeg, .png, .gif)')])
+    image = FileField("商品照片", validators=[Optional(), FileAllowed(['jpeg', 'jpg', 'png', 'gif'], '只能上傳圖片(.jpg, .jpeg, .png, .gif)')])
     name = StringField("商品名稱", validators=[InputRequired("商品名稱不得為空"), Length(max=30, message="商品名稱不得超過30個字")])
     price = IntegerField("商品價格", validators=[InputRequired("商品價格不得為空"), NumberRange(min=1, max=100000, message="商品價格介於1~100000")])
     detail = CKEditorField("商品詳情")
